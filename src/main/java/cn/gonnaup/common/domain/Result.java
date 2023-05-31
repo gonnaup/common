4c28af0b7e4ea9d4fa0e9df3f6d55006d47239ff
gonnaup <gonnaup@163.com>
2023-05-31 18:43:41 +0800

    创建公共返回模型

---
+++ src/main/java/cn/gonnaup/common/domain/Result.java
@@ -0,0 +1,57 @@
+package cn.gonnaup.common.domain;
+
+/**
+ * 公共返回模型
+ *
+ * @author gonnaup
+ * @version created at 2023/5/31 下午5:45
+ */
+public record Result<T>(T data, String code, String message) {
+
+    /**
+     * 请求成功时返回结果
+     *
+     * @param data 返回数据
+     * @param <T>
+     * @return 成功结果模型
+     */
+    public static <T> Result<T> ok(T data) {
+        return new Result<>(data, Code.Success.code, Code.Success.description);
+    }
+
+    /**
+     * 请求失败时返回结果
+     *
+     * @param reason 失败原因
+     * @return 失败结果模型
+     */
+    public static Result<String> fail(String reason) {
+        return new Result<>("", Code.Fail.code, reason);
+    }
+
+    /**
+     * 返回码，待完善
+     */
+    enum Code {
+
+        Success("200", "success"),
+        Fail("400", "fail");
+
+        private final String code;
+
+        private final String description;
+
+        Code(String code, String description) {
+            this.code = code;
+            this.description = description;
+        }
+
+        public String code() {
+            return code;
+        }
+
+        public String description() {
+            return description;
+        }
+    }
+}
---
+++ src/test/java/cn/gonnaup/common/domain/ResultTest.java
@@ -0,0 +1,31 @@
+package cn.gonnaup.common.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author gonnaup
+ * @version created at 2023/5/31 下午6:03
+ */
+public class ResultTest {
+
+    record Person(String name, int age) {
+    }
+
+    @Test
+    void resultOkTest() {
+        Person mike = new Person("mike", 20);
+        Result<Person> ok = Result.ok(mike);
+        Assertions.assertSame(ok.data(), mike);
+        System.out.println(ok);
+    }
+
+    @Test
+    void resultFailTest() {
+        String reason = "error.account.notexist";
+        Result<String> fail = Result.fail(reason);
+        Assertions.assertEquals(fail.message(), reason);
+        System.out.println(fail);
+    }
+
+}
