788d9cb63bade631940f530a2e54485219c6e401
gonnaup <gonnaup@163.com>
2024-03-10 11:10:51 +0800

    feat(可执行包打包示例):

---
+++ src/main/java/org/gonnaup/common/Main.java
@@ -0,0 +1,13 @@
+package org.gonnaup.common;
+
+/**
+ * 打可执行jar包示例Main方法
+ *
+ * @author gonnaup
+ * @version created at 2024/3/10 上午10:11
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
